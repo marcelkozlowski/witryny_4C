5ad30f126b9ac98a54f89766c4a7ad317fd1793c
marcelkozlowski <144424736+marcelkozlowski@users.noreply.github.com>
2023-09-15 09:54:43 +0200

    Add files via upload

---
+++ syntax.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Tu funkcja echo.";
+    ?>
+    <br>
+    <?php echo 'Tu funkcja echo';
+    ?>
+    <br>
+    <?php
+    echo 'Tu', 'funkcja', 'echo.';
+    ?>
+    <br>
+    <?php
+    echo 'Tu' . 'funkcja' . 'echo'
+    ?>
+</body>
+</html>
