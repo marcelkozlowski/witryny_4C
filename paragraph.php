b698bd32c29932de6a97116fede81680b04bbdd0
marcelkozlowski <144424736+marcelkozlowski@users.noreply.github.com>
2023-09-22 10:21:31 +0200

    Add files via upload

---
+++ paragraph.php
@@ -0,0 +1,16 @@
+    <?php
+    $a = 97;
+    $b = 3.14;
+    $c = 'PHP';
+    ?>
+    <h3 style="color:red"=$a</h3>
+
+
+    <?php
+    $poczatek = 'Ala ma ';
+    $liczba = 123;
+    $koniec = 'koty!';
+    echo '$poczatek $liczba $koniec';
+    echo "$poczatek $liczba $koniec";
+
+
---
+++ zmienna.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $title='Strona Główna';
+?>
+<h1><?php echo $title;?></h1>
+
+    <ol>
+        <li><a href="https://www.youtube.com/">YouTube</a></li>
+        <li><a href="https://twitter.com/?lang=pl/">Twitter</a></li>
+        <li><a href="https://www.facebook.com/?locale=pl_PL/">Facebook</a></li>
+</ol>
+   
+</body>
+</html>
